2aecc0f35712cae0e2c34d3c8c74d76f5ea21e4d
Abhishekh prajapati <abhishekhprajapati9@gmail.com>
2026-09-07 16:27:22 +0530

    Time: 27 ms (78.64%), Space: 12.5 MB (88.64%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        count = {}
+
+        for i in magazine:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+
+        for i in ransomNote:
+            if i not in count or count[i] == 0:
+                return False
+            count[i] -= 1
+            
+        return True
+                
+
+
+
+
+        
